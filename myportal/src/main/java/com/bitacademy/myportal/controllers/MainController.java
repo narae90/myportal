029c7695276f56073dab5d2e7b5e6f939dc13e1c
김나래 <narae900@naver.com>
2021-09-28 08:50:23 +0900

    .

--- myportal/src/main/java/com/bitacademy/myportal/controllers/MainController.java
+++ myportal/src/main/java/com/bitacademy/myportal/controllers/MainController.java
@@ -7,20 +7,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
-	// 메소드가 /hello URL 요청에 반응하도록 한다.
-	@RequestMapping("/home")
-	public ModelAndView home(@RequestParam String name) {
-		// 서블릿에서 getParameter로 해준 것을 RequestParam이 대신 해준 것
+	@RequestMapping({"/", "/main"})
+	public ModelAndView home() {
 		ModelAndView mav = new ModelAndView();
-		
-		// 객체 추가: 서블릿에서 addAttribute의 역할
-		mav.addObject("message", // 키
-						"Hello," + name); // 값
-		
-		// 뷰파일을 연결 : 서블릿에서 RequestDispather로 포워드한 것과 비슷
 		mav.setViewName("/WEB-INF/views/home.jsp");
-		
 		return mav;
 	}
-
 }
